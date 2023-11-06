5b38a982edcd622dfbba99864897d6f646b90b4c
Nguyen <lenguyenhcm325@gmail.com>
2023-11-06 23:06:15 +0100

    add prompt login when an unauthenticated user click add to cart

--- src/components/add-to-cart-button/add-to-cart-button.component.jsx
+++ src/components/add-to-cart-button/add-to-cart-button.component.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect, useRef, Fragment} from "react";
+import PromptLoginAlert from "../prompt-login-alert/prompt-login-alert.component";
 import { AddToCartButtonContainer } from "./add-to-cart-button.styles";
 import { SVGasImage } from "../svg-block/svg-block.styles";
 import { handleAddItemToCartOnFS } from "../../store/cart/cart.thunk";
@@ -6,14 +7,50 @@ import { selectCurrentUser } from "../../store/user/user.selector";
 import { useDispatch, useSelector } from "react-redux";
 const AddToCartButton = ({image_url, model_brand, price, description}) => {
     const currentUser = useSelector(selectCurrentUser);
+    const timerRef = useRef(null);
+    const [showPromptLogin, setShowPromptLogin] = useState(false);
     const dispatch = useDispatch(); 
 
+    const handleClick = () => {
+        {
+            if (!currentUser){
+                if (timerRef.current){
+                    clearTimeout(timerRef.current)
+                }
+                setShowPromptLogin(true);
+                timerRef.current = setTimeout(() => {
+                    setShowPromptLogin(false)
+                }, 2000)
+            }
+            else {
+                dispatch(handleAddItemToCartOnFS(currentUser.uid, {image_url, model_brand, price, description}))
+            }
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            if (timerRef.current){
+                clearTimeout(timerRef.current)
+            }
+        }
+    }, [])
+
     return (
-        <AddToCartButtonContainer onClick={() => dispatch(handleAddItemToCartOnFS(currentUser.uid, {image_url, model_brand, price, description}))}>
-            <SVGasImage src="https://toycars-img.s3.eu-central-1.amazonaws.com/svg/plus-svg.svg" alt="" /> <span>Add to cart</span>
-        </AddToCartButtonContainer>
+        <Fragment>
+            {
+                showPromptLogin && (<PromptLoginAlert/>)
+            }
+
+            <AddToCartButtonContainer onClick={handleClick}>
+                <SVGasImage src="https://toycars-img.s3.eu-central-1.amazonaws.com/svg/plus-svg.svg" alt="" /> <span>Add to cart</span>
+
+            </AddToCartButtonContainer>
+        </Fragment>
     )
 
 }
 
 export default AddToCartButton
+
+// () => dispatch(handleAddItemToCartOnFS(currentUser.uid, {image_url, model_brand, price, description}))
